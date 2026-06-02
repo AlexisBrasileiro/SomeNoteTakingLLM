0a27941cc4d06939969d6b771cd96de3029782a1
Alexis Brasileiro <Alexis.Brasileiro@outlook.com>
2026-06-02 19:05:00 -0300

    melhoria nos nomes dos meses

--- SRC/sntllm-web/src/components/Sidebar.tsx
+++ SRC/sntllm-web/src/components/Sidebar.tsx
@@ -25,8 +25,8 @@ function buildTree(notes: Note[]): NoteWithChildren[] {
 }
 
 const MONTH_NAMES = [
-  'Janeiro','Fevereiro','Março','Abril','Maio','Junho',
-  'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro',
+  '01-Janeiro','02-Fevereiro','03-Março','04-Abril','05-Maio','06-Junho',
+  '07-Julho','08-Agosto','09-Setembro','10-Outubro','11-Novembro','12-Dezembro',
 ]
 
 // isCalendarContainer: nota com título no formato "DD/MM/YYYY" e sem pai
